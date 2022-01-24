b09dddb2d246780a07a13c55d94303fadc5f3b2b
Jacques857 <jleongsi@ualberta.ca>
2022-01-24 10:52:14 -0700

    cleanup

--- plasmapperAPI/plasmapperAPI/settings.py
+++ plasmapperAPI/plasmapperAPI/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e$=vq0u0i-gfv$oes*8t=lz@x!!1cy&x_ok(&m1&4@xhmb!*i@'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
