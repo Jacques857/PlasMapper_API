7381e09723bb23e37f0de5df00624b0133e64b3b
Jacques857 <jleongsi@ualberta.ca>
2022-02-01 17:11:49 -0700

    removed unused functions from Utility.py

--- plasmapperAPI/fsdSearch/Utility.py
+++ plasmapperAPI/fsdSearch/Utility.py
@@ -5,10 +5,3 @@ class Utility(ABC): # abstract class
     # Returns the value of the specified key in .env
     def getEnvValue(key):
         return config(key)
-        
-    def readString():
-        pass
-    def removeUselessCharacters():
-        pass
-    def formatToFasta():
-        pass
