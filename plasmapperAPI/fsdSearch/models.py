34a0332fcd3f184b0202283ba1fce8773c6300e8
Jacques857 <jleongsi@ualberta.ca>
2022-02-15 13:34:29 -0700

    db migrations

--- plasmapperAPI/fsdSearch/models.py
+++ plasmapperAPI/fsdSearch/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Plasmid(models.Model):
+    name = models.CharField(max_length=200)
+    sequence = models.CharField(max_length=50000)
+    sequenceLength = models.IntegerField()
+    backbone = models.CharField(max_length=200)
+    features = models.CharField(max_length=1500) # a string containing feature names seperated by commas
