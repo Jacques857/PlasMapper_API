d58e6266465f20b7d83433886524f7cfb9fc78f8
Jacques857 <jleongsi@ualberta.ca>
2022-02-15 14:07:06 -0700

    remove Plasmid model

--- plasmapperAPI/fsdSearch/admin.py
+++ plasmapperAPI/fsdSearch/admin.py
@@ -1,5 +1,1 @@
 from django.contrib import admin
-
-from .models import Plasmid
-
-admin.site.register(Plasmid)
--- plasmapperAPI/fsdSearch/models.py
+++ plasmapperAPI/fsdSearch/models.py
@@ -1,8 +1,1 @@
 from django.db import models
-
-class Plasmid(models.Model):
-    name = models.CharField(max_length=200)
-    sequence = models.CharField(max_length=50000)
-    sequenceLength = models.IntegerField()
-    backbone = models.CharField(max_length=200)
-    features = models.CharField(max_length=1500) # a string containing feature names seperated by commas
