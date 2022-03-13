a3cd0ff500c2698762517f1e009b6b0244aa7f0e
root <root@gce-plasmapper-1.c.wishartlab-gce.internal>
2022-03-13 20:47:00 +0000

    set up backend for deployment and fixed wordsize issue for feature search

--- plasmapperAPI/plasmapperAPI/settings.py
+++ plasmapperAPI/plasmapperAPI/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
@@ -52,7 +52,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000",
+    "http://34.122.111.83",
 ]
 
 ROOT_URLCONF = 'plasmapperAPI.urls'
